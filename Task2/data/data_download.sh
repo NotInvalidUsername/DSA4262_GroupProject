2bef03ba06bf495983196e8771f597fcf184d607
NotInvalidUsername <ryannjh99@gmail.com>
2025-11-03 15:45:29 +0800

    Add data_download.sh to fetch RNA sequencing data

---
+++ Task2/data/data_download.sh
@@ -0,0 +1,64 @@
+# === A549 (lung cancer) ===
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_A549_directRNA_replicate5_run1/data.json SGNex_A549_directRNA_replicate5_run1.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_A549_directRNA_replicate5_run1/data.index SGNex_A549_directRNA_replicate5_run1.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_A549_directRNA_replicate5_run1/data.info SGNex_A549_directRNA_replicate5_run1.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_A549_directRNA_replicate5_run1/data.readcount SGNex_A549_directRNA_replicate5_run1.readcount
+
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_A549_directRNA_replicate6_run1/data.json SGNex_A549_directRNA_replicate6_run1.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_A549_directRNA_replicate6_run1/data.index SGNex_A549_directRNA_replicate6_run1.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_A549_directRNA_replicate6_run1/data.info SGNex_A549_directRNA_replicate6_run1.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_A549_directRNA_replicate6_run1/data.readcount SGNex_A549_directRNA_replicate6_run1.readcount
+
+# === Hct116 (colon cancer) ===
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate3_run1/data.json SGNex_Hct116_directRNA_replicate3_run1.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate3_run1/data.index SGNex_Hct116_directRNA_replicate3_run1.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate3_run1/data.info SGNex_Hct116_directRNA_replicate3_run1.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate3_run1/data.readcount SGNex_Hct116_directRNA_replicate3_run1.readcount
+
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate3_run4/data.json SGNex_Hct116_directRNA_replicate3_run4.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate3_run4/data.index SGNex_Hct116_directRNA_replicate3_run4.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate3_run4/data.info SGNex_Hct116_directRNA_replicate3_run4.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate3_run4/data.readcount SGNex_Hct116_directRNA_replicate3_run4.readcount
+
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate4_run3/data.json SGNex_Hct116_directRNA_replicate4_run3.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate4_run3/data.index SGNex_Hct116_directRNA_replicate4_run3.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate4_run3/data.info SGNex_Hct116_directRNA_replicate4_run3.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_Hct116_directRNA_replicate4_run3/data.readcount SGNex_Hct116_directRNA_replicate4_run3.readcount
+
+# === HepG2 (liver cancer) ===
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_HepG2_directRNA_replicate5_run2/data.json SGNex_HepG2_directRNA_replicate5_run2.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_HepG2_directRNA_replicate5_run2/data.index SGNex_HepG2_directRNA_replicate5_run2.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_HepG2_directRNA_replicate5_run2/data.info SGNex_HepG2_directRNA_replicate5_run2.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_HepG2_directRNA_replicate5_run2/data.readcount SGNex_HepG2_directRNA_replicate5_run2.readcount
+
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_HepG2_directRNA_replicate6_run1/data.json SGNex_HepG2_directRNA_replicate6_run1.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_HepG2_directRNA_replicate6_run1/data.index SGNex_HepG2_directRNA_replicate6_run1.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_HepG2_directRNA_replicate6_run1/data.info SGNex_HepG2_directRNA_replicate6_run1.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_HepG2_directRNA_replicate6_run1/data.readcount SGNex_HepG2_directRNA_replicate6_run1.readcount
+
+# === MCF7 (breast cancer) ===
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_MCF7_directRNA_replicate3_run1/data.json SGNex_MCF7_directRNA_replicate3_run1.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_MCF7_directRNA_replicate3_run1/data.index SGNex_MCF7_directRNA_replicate3_run1.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_MCF7_directRNA_replicate3_run1/data.info SGNex_MCF7_directRNA_replicate3_run1.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_MCF7_directRNA_replicate3_run1/data.readcount SGNex_MCF7_directRNA_replicate3_run1.readcount
+
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_MCF7_directRNA_replicate4_run1/data.json SGNex_MCF7_directRNA_replicate4_run1.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_MCF7_directRNA_replicate4_run1/data.index SGNex_MCF7_directRNA_replicate4_run1.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_MCF7_directRNA_replicate4_run1/data.info SGNex_MCF7_directRNA_replicate4_run1.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_MCF7_directRNA_replicate4_run1/data.readcount SGNex_MCF7_directRNA_replicate4_run1.readcount
+
+# === K562 (chronic myeloid leukemia / blood cancer) ===
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate4_run1/data.json SGNex_K562_directRNA_replicate4_run1.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate4_run1/data.index SGNex_K562_directRNA_replicate4_run1.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate4_run1/data.info SGNex_K562_directRNA_replicate4_run1.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate4_run1/data.readcount SGNex_K562_directRNA_replicate4_run1.readcount
+
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate5_run1/data.json SGNex_K562_directRNA_replicate5_run1.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate5_run1/data.index SGNex_K562_directRNA_replicate5_run1.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate5_run1/data.info SGNex_K562_directRNA_replicate5_run1.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate5_run1/data.readcount SGNex_K562_directRNA_replicate5_run1.readcount
+
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate6_run1/data.json SGNex_K562_directRNA_replicate6_run1.json
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate6_run1/data.index SGNex_K562_directRNA_replicate6_run1.index
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate6_run1/data.info SGNex_K562_directRNA_replicate6_run1.info
+aws s3 cp --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/SGNex_K562_directRNA_replicate6_run1/data.readcount SGNex_K562_directRNA_replicate6_run1.readcount
